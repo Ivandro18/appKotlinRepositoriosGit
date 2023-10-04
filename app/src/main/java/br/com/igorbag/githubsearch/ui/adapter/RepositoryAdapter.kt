4781258c137f0f23316865370ac2459a4110b057
ivandro18 <ivandro.m.silva18@gmail.com>
2023-10-04 01:54:12 -0300

    Conclusão do projeto

---
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/adapter/RepositoryAdapter.kt
@@ -0,0 +1,88 @@
+package br.com.igorbag.githubsearch.ui.adapter
+
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import br.com.igorbag.githubsearch.R
+import br.com.igorbag.githubsearch.domain.Repository
+import br.com.igorbag.githubsearch.ui.MainActivity
+
+
+class RepositoryAdapter(private val repositories: List<Repository>) :
+    RecyclerView.Adapter<RepositoryAdapter.ViewHolder>() {
+
+    var repositoryItemLister: (Repository) -> Unit = {}
+    var btnShareLister: (Repository) -> Unit = {}
+
+    // Cria uma nova view
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.repository_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    // Pega o conteudo da view e troca pela informacao de item de uma lista
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //Exemplo de Bind
+        //  holder.preco.text = repositories[position].atributo
+
+        // Exemplo de click no item
+        //holder.itemView.setOnClickListener {
+        // carItemLister(repositores[position])
+        //}
+
+        // Exemplo de click no btn Share
+        //holder.favorito.setOnClickListener {
+        //    btnShareLister(repositores[position])
+        //}
+
+        holder.nome.text = repositories[position].name
+        holder.nome.setOnClickListener{
+            repositoryItemLister(repositories[position])
+        }
+        holder.compartilhar.setOnClickListener{
+            btnShareLister(repositories[position])
+
+
+        }
+    }
+
+    // Pega a quantidade de repositorios da lista
+    override fun getItemCount(): Int = repositories.size
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        //Exemplo:
+        //val atributo: TextView
+
+        //init {
+        //    view.apply {
+        //        atributo = findViewById(R.id.item_view)
+        //    }
+
+        val nome : TextView
+        val compartilhar : ImageView
+
+        init {
+            view.apply {
+                nome = findViewById(R.id.tv_repository)
+                compartilhar = findViewById(R.id.iv_shared)
+            }
+        }
+
+
+    }
+
+
+
+
+}
+
+
+
